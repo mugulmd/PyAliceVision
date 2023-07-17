9621649319e2d5fdf4808746e96920a4e8172cad
Loïc Vital <mugulmotion@gmail.com>
2023-07-17 18:13:18 +0200

    load and save SfMData

--- src/pyalicevision.cpp
+++ src/pyalicevision.cpp
@@ -1,18 +1,33 @@
 #include <pybind11/pybind11.h>
 
-#include <aliceVision/sfmData/View.hpp>
+#include <aliceVision/sfmData/SfMData.hpp>
+#include <aliceVision/sfmDataIO/sfmDataIO.hpp>
 
+#include <stdexcept>
 #include <string>
 
 namespace py = pybind11;
 using namespace aliceVision;
 
 
-PYBIND11_MODULE(pyalicevision, m)
-{
+PYBIND11_MODULE(pyalicevision, m) {
     m.doc() = "Python binding for AliceVision";
 
     py::class_<sfmData::View>(m, "View")
         .def(py::init<const std::string &>())
         .def("getImagePath", &sfmData::View::getImagePath);
+    
+    py::class_<sfmData::SfMData>(m, "SfMData")
+        .def(py::init([](const std::string & filename) {
+            sfmData::SfMData self;
+            if (!sfmDataIO::Load(self, filename, sfmDataIO::ESfMData::ALL)) {
+                throw std::runtime_error("Failed to load SfMData file.");
+            }
+            return self;
+        }))
+        .def("save", [](const sfmData::SfMData & self, const std::string & filename) {
+            if (!sfmDataIO::Save(self, filename, sfmDataIO::ESfMData::ALL)) {
+                throw std::runtime_error("Failed to load SfMData file.");
+            }
+        });
 }
