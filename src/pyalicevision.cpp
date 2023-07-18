b4f954c797ac03e877c4b6839ca5c09413548606
Loïc Vital <mugulmotion@gmail.com>
2023-07-18 15:42:01 +0200

    complete View properties

--- src/pyalicevision.cpp
+++ src/pyalicevision.cpp
@@ -21,7 +21,14 @@ PYBIND11_MODULE(pyalicevision, m) {
 
     py::class_<sfmData::View, std::shared_ptr<sfmData::View>>(m, "View")
         .def(py::init<>())
-        .def_property("path", &sfmData::View::getImagePath, &sfmData::View::setImagePath);
+        .def_property("path", &sfmData::View::getImagePath, &sfmData::View::setImagePath)
+        .def_property("width", &sfmData::View::getWidth, &sfmData::View::setWidth)
+        .def_property("height", &sfmData::View::getHeight, &sfmData::View::setHeight)
+        .def_property("viewId", &sfmData::View::getViewId, &sfmData::View::setViewId)
+        .def_property("intrinsicId", &sfmData::View::getIntrinsicId, &sfmData::View::setIntrinsicId)
+        .def_property("poseId", &sfmData::View::getPoseId, &sfmData::View::setPoseId)
+        .def_property("frameId", &sfmData::View::getFrameId, &sfmData::View::setFrameId)
+        .def_property_readonly("metadata", [](std::shared_ptr<sfmData::View> self) { return self->getMetadata(); });
     
     py::bind_map<sfmData::Views>(m, "Views");
     
@@ -29,13 +36,13 @@ PYBIND11_MODULE(pyalicevision, m) {
         .def(py::init<>())
         .def(py::init([](const std::string & filename) {
             sfmData::SfMData self;
-            if (!sfmDataIO::Load(self, filename, sfmDataIO::ESfMData::ALL)) {
+            if (!sfmDataIO::Load(self, filename, sfmDataIO::ESfMData(sfmDataIO::ALL))) {
                 throw std::runtime_error("Failed to load SfMData file.");
             }
             return self;
         }))
         .def("save", [](const sfmData::SfMData & self, const std::string & filename) {
-            if (!sfmDataIO::Save(self, filename, sfmDataIO::ESfMData::ALL)) {
+            if (!sfmDataIO::Save(self, filename, sfmDataIO::ESfMData(sfmDataIO::ALL))) {
                 throw std::runtime_error("Failed to load SfMData file.");
             }
         })
