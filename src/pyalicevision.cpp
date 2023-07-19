e43064a0ac785cc9264e84fceddfc5cdc2f31c48
Loïc Vital <mugulmotion@gmail.com>
2023-07-19 14:36:58 +0200

    add landmarks

--- src/pyalicevision.cpp
+++ src/pyalicevision.cpp
@@ -19,6 +19,7 @@ using namespace aliceVision;
 PYBIND11_MAKE_OPAQUE(sfmData::Views)
 PYBIND11_MAKE_OPAQUE(sfmData::Poses)
 PYBIND11_MAKE_OPAQUE(sfmData::Intrinsics)
+PYBIND11_MAKE_OPAQUE(sfmData::Landmarks)
 
 
 PYBIND11_MODULE(pyalicevision, m) {
@@ -60,9 +61,13 @@ PYBIND11_MODULE(pyalicevision, m) {
         .def_property("width", &camera::IntrinsicBase::w, &camera::IntrinsicBase::setWidth)
         .def_property("height", &camera::IntrinsicBase::h, &camera::IntrinsicBase::setHeight);
     
+    py::class_<sfmData::Landmark>(m, "Landmark")
+        .def_readwrite("X", &sfmData::Landmark::X);
+    
     py::bind_map<sfmData::Views>(m, "Views");
     py::bind_map<sfmData::Poses>(m, "Poses");
     py::bind_map<sfmData::Intrinsics>(m, "Intrinsics");
+    py::bind_map<sfmData::Landmarks>(m, "Landmarks");
     
     py::class_<sfmData::SfMData>(m, "SfMData")
         .def(py::init<>())
@@ -84,5 +89,6 @@ PYBIND11_MODULE(pyalicevision, m) {
         .def("poses",
             static_cast<const sfmData::Poses & (sfmData::SfMData::*)() const>
                 (&sfmData::SfMData::getPoses))
-        .def_readwrite("intrinsics", &sfmData::SfMData::intrinsics);
+        .def_readwrite("intrinsics", &sfmData::SfMData::intrinsics)
+        .def_readwrite("structure", &sfmData::SfMData::structure);
 }
