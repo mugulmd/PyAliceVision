7c9a4c90772db4d684992d84156070384d319d54
Loïc Vital <mugulmotion@gmail.com>
2023-07-20 10:02:21 +0200

    [camera] expose intrinsic type

--- src/camera.cpp
+++ src/camera.cpp
@@ -24,7 +24,8 @@ void bind_camera(py::module & m) {
 
     py::class_<camera::IntrinsicBase, std::shared_ptr<camera::IntrinsicBase>>(m, "IntrinsicBase")
         .def_property("width", &camera::IntrinsicBase::w, &camera::IntrinsicBase::setWidth)
-        .def_property("height", &camera::IntrinsicBase::h, &camera::IntrinsicBase::setHeight);
+        .def_property("height", &camera::IntrinsicBase::h, &camera::IntrinsicBase::setHeight)
+        .def("intrinsicType", &camera::IntrinsicBase::getType);
     
     m.def("createIntrinsic", &camera::createIntrinsic);
 }
