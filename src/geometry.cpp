9290d8be0a019f0e59bca833a9476283765abfc7
Loïc Vital <mugulmotion@gmail.com>
2023-09-29 12:02:24 +0200

    [geometry] update Pose3 binding

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -12,8 +12,6 @@ void bind_geometry(py::module & m) {
     // Represents a 3D rigid transform
     py::class_<geometry::Pose3>(m, "Pose3")
         .def(py::init<>())
-        .def("rotation",
-            static_cast<const Mat3 & (geometry::Pose3::*)() const>(&geometry::Pose3::rotation))
-        .def("center",
-            static_cast<const Vec3 & (geometry::Pose3::*)() const>(&geometry::Pose3::center));
+        .def("rotation", &geometry::Pose3::rotation)
+        .def("center", &geometry::Pose3::center);
 }
